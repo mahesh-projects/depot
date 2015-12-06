06918d7eff653b380fc054adaa30a78eafd7f19a
Mahesh Shivamallappa <mahesh.shivamallappa@myob.com>
2015-12-06 14:37:59 +1100

    Count & display number of catalog views

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
+  
+  before_action :set_counter, only: [:index]		
+
   def index
   	@products = Product.order(:title)
   end
+
+  private 
+  	def set_counter
+		if session[:counter].nil?
+			session[:counter] = 1
+			@counter = session[:counter]
+		else
+			session[:counter] = session[:counter] + 1
+			@counter = session[:counter]
+		end
+	end
 end
