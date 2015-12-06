d39e1e6c4956b2627ada6eb25a5dc2efb4c5681f
Mahesh Shivamallappa <mahesh.shivamallappa@myob.com>
2015-12-06 20:12:36 +1100

    Count views and reset counter

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -5,8 +5,16 @@ module CurrentCart
 		
 		def set_cart
 			@cart = Cart.find(session[:cart_id])
+			reset_counter
 		rescue ActiveRecord::RecordNotFound
 			@cart = Cart.create
 			session[:cart_id] = @cart.id
+			reset_counter
+		end
+
+		def reset_counter
+			unless session[:counter].nil?
+				session[:counter] = 0
+			end
 		end
 end
